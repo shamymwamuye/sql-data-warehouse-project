552ada71cc4d5bef09d776b5cdfab932906bfdfd
Shamy Mwamuye <mwamuyeshamy@gmail.com>
2026-08-13 20:02:24 +0300

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -2,10 +2,10 @@
 ==================================================================================================
 DDL Script: Create Bronze Tables
 ==================================================================================================
-Script Pupose:
+Script Purpose:
 
 This script creates tables in the 'bronze' schema, dropping existing tables if they already exist.
-Run this script to re-define the DDL structure of 'bronze' tables
+Run this script to redefine the DDL structure of 'bronze' tables
 ==================================================================================================
 */
 
